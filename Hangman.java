7aa39477e3e51f8b192da3a02fefc4c72a936a33
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-11-06 08:02:45 -0600

    Final Commit
    The hangman game is fully functional and ready to be played from with in the terminal.

--- Hangman.java
+++ Hangman.java
@@ -3,9 +3,10 @@ public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("Scot");
         Prompter prompter = new Prompter(game);
-        while (game.getRemainingTries() > 0) {
+        while (game.getRemainingTries() > 0 && !game.isWon()) {
             prompter.displayProgress();
             prompter.promptForGuess();
         }
+        prompter.displayOutcome();
     }
 }
--- Prompter.java
+++ Prompter.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Prompter {
+public class Prompter {
     private Game game;
 
     public Prompter(Game game) {
@@ -9,14 +9,32 @@ class Prompter {
 
     public boolean promptForGuess() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a letter:  ");
-        String guessInput = scanner.nextLine();
-        char guess = guessInput.charAt(0);
-        return game.applyGuess(guess);
+        boolean isHit = false;
+        boolean isAccepted = false;
+
+        do {
+            System.out.print("Enter a letter:  ");
+            String guessInput = scanner.nextLine();
+            try {
+                isHit = game.applyGuess(guessInput);
+                isAccepted = true;
+            } catch(IllegalArgumentException iae){
+                System.out.printf("%s, please try again.", iae.getMessage());
+            }
+        } while(!isAccepted);
+        return isHit;
     }
 
     public void displayProgress() {
         System.out.printf("You have %d tries left to solve: %s%n", game.getRemainingTries(), game.getCurrentProgress());
+    }
+    public void displayOutcome(){
+        if(game.isWon()){
+            System.out.printf("Congrats you with with %d tries remaining%n",game.getRemainingTries());
+        } else {
+            System.out.printf("Bummer, the word was %s %n", game.getAnswer());
+
+        }
     }
 
 }
